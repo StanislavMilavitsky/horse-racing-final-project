95506de6d4a3219389866fa814d609b81d4c6bf7
Acer <stanislav.milavitsky@gmail.com>
2021-10-26 16:51:10 +0300

    rename

--- src/by/milavitsky/horseracing/cache/CacheVariable.java
+++ src/by/milavitsky/horseracing/cache/CacheVariable.java
@@ -1,7 +1,12 @@
 package by.milavitsky.horseracing.cache;
 
+/**
+ * The type Cache variables.
+ */
 public class CacheVariables {
-
+    /**
+     * The key in cache map.
+     */
     public static final String COUNT_ACTIVE = "countActive";
     public static final String COUNT_ALL = "countAll";
     public static final String USER_ROLES = "roles";
